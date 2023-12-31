fb8c5ce7fc28e6f18b1d27a0850fb3c497087cf7
apacheco <j.alfredo.pacheco@gmail.com>
2023-12-31 01:29:08 -0600

    fix email verification page

--- src/common/Account/EmailVerification/index.tsx
+++ src/common/Account/EmailVerification/index.tsx
@@ -6,6 +6,7 @@ const VerifyEmail = () => {
   if (error)
     return (
       <div className="container max-w-sm mx-auto flex flex-col items-center gap-5 h-full justify-center">
+        <Logo width="150" height="150" />
         <h1 className="text-2xl font-bold">Email Verification Failed!</h1>
         <p className="text-center">Invalid Link or Token expired </p>
         <p className="text-center">
@@ -17,6 +18,7 @@ const VerifyEmail = () => {
   if (verified)
     return (
       <div className="container max-w-sm mx-auto flex flex-col items-center gap-5 h-full justify-center">
+        <Logo width="150" height="150" />
         <h1 className="text-2xl font-bold">Email Verified Successfully!</h1>
         <p className="text-center">
           You will be redirected to the login page in 5 seconds.
@@ -26,7 +28,7 @@ const VerifyEmail = () => {
 
   return (
     <div className="container max-w-sm mx-auto flex flex-col items-center gap-5 h-full justify-center">
-      <Logo width="250" height="150" />
+      <Logo width="150" height="150" />
       <p>Verifying Email...</p>
     </div>
   );
