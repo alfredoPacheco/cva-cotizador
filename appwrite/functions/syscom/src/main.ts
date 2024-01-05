8a08b72ee70d4aade50f2e46e7df02f5dbefc48b
apacheco <j.alfredo.pacheco@gmail.com>
2024-01-04 21:33:57 -0600

    syscom fn in progress

---
+++ appwrite/functions/syscom/src/main.ts
@@ -0,0 +1,301 @@
+import { get } from 'lodash';
+import { Client, Databases, Query, Functions } from 'node-appwrite';
+
+interface CategoryDto {
+  id: string;
+  name: string;
+  level: number;
+  productId: string;
+}
+
+interface BrandDto {
+  $id?: string;
+  name: string;
+}
+
+interface ProductDto {
+  // $id: string;
+  raw: string;
+  productId: string;
+  model: string;
+  totalStock: number;
+  title: string;
+  brand: string;
+  satKey: string;
+  imgMain: string;
+  privateLink: string;
+  categories: CategoryDto[];
+  pvol: string;
+  brandLogo: string;
+  link: string;
+  icons: string;
+  weight: string;
+  existence: string;
+  uom: string;
+  height: string;
+  large: string;
+  width: string;
+  prices: string;
+  note: string;
+  attributes: string;
+}
+
+const client = new Client()
+  .setEndpoint('https://app.do.inspiracode.com/v1')
+  // @ts-ignore
+  .setProject(Bun.env['APPWRITE_FUNCTION_PROJECT_ID'])
+  // @ts-ignore
+  .setKey(Bun.env['APPWRITE_API_KEY']);
+
+const databases = new Databases(client);
+const functions = new Functions(client);
+
+const main = async ({ req, res, log, error }: any) => {
+  const startTime = new Date().getTime();
+  let currentTime = new Date().getTime();
+
+  // @ts-ignore
+  let startPage = Number(Bun.env['START_PAGE'] || 1);
+  let startBrand;
+
+  try {
+    startPage = JSON.parse(req.body).startPage;
+    log('startPage from request: ' + startPage);
+  } catch {}
+
+  try {
+    startBrand = JSON.parse(req.body).startBrand;
+    log('startBrand from request: ' + startBrand);
+  } catch {}
+
+  // @ts-ignore
+  const TOKEN = Bun.env['TOKEN'];
+  const brandUrl = 'https://developers.syscom.mx/api/v1/marcas';
+
+  const existingBrands = await databases.listDocuments('syscom', 'brands', [
+    Query.limit(999999999),
+    Query.select(['$id'])
+  ]);
+
+  const response = await fetch(brandUrl, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+  });
+
+  const json = await response.json();
+
+  // log(JSON.stringify(json, null, 2));
+
+  const brands = json;
+
+  if (!startBrand) {
+    log('going to process brands');
+    for await (const brand of brands) {
+      const id = get(brand, 'id').toString();
+      const payload: BrandDto = {
+        name: get(brand, 'nombre')
+      };
+
+      log('payload');
+      log(payload);
+
+      if (existingBrands.documents.some((doc: any) => doc.$id === id)) {
+        log('updating brand: ' + id);
+        await databases.updateDocument('syscom', 'brands', id, payload);
+      } else {
+        log('creating product: ' + id);
+        await databases.createDocument('syscom', 'brands', id, payload);
+      }
+    }
+  }
+  // END BRANDS
+
+  const existingProducts = await databases.listDocuments('syscom', 'products', [
+    Query.limit(999999999),
+    Query.select(['$id'])
+  ]);
+
+  const baseUrl = new URL('https://api.tvc.mx/products');
+
+  let CURRENT_PAGE = startPage;
+  const PER_PAGE = '100';
+  const MAX_LOOP_TIMES = 999999999;
+  // const MAX_LOOP_TIMES = 1;
+  let LOOP_COUNT = 1;
+  let ATTEMPTS_COUNT = 1;
+  const MAX_ATTEMPTS = 3;
+
+  let hasNextPage = false;
+
+  let params = {
+    // tvcIds: [31071, 148],
+    // tvcModels: ['RBM0220002', '42109'],
+    withInventory: 'detailed',
+    withPrice: true,
+    withMedia: true,
+    withOverviews: false,
+    withWeightsAndDimensions: true,
+    withCategoryBreadcrumb: true,
+    perPage: PER_PAGE,
+    page: CURRENT_PAGE.toString()
+  };
+  Object.keys(params).forEach(key =>
+    baseUrl.searchParams.append(key, params[key])
+  );
+
+  do {
+    try {
+      const url = new URL(baseUrl);
+      url.searchParams.set('page', CURRENT_PAGE.toString());
+      // url.searchParams.append('tvcIds[]', '4804');
+
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        }
+      });
+
+      const json = await response.json();
+
+      // log(JSON.stringify(json, null, 2));
+
+      const products = json.data;
+
+      for await (const product of products) {
+        log(
+          'goint to process page: ' +
+            CURRENT_PAGE +
+            ', LOOP_COUNT: ' +
+            LOOP_COUNT
+        );
+        // const payload: ProductDto = {
+        const payload = {
+          // $id: get(product, 'tvc_id').toString(),
+          raw: JSON.stringify(product),
+          tvcModel: get(product, 'tvc_model'),
+          providerModel: get(product, 'provider_model'),
+          tvcId: get(product, 'tvc_id'),
+          name: get(product, 'name'),
+          type: get(product, 'type'),
+          brand: get(product, 'brand'),
+          brandId: get(product, 'brand_id'),
+          categoryId: get(product, 'category_id'),
+          category: get(product, 'category'),
+          // categoryBreadcrumb: get(product, 'category_breadcrumb'),
+          satKey: get(product, 'sat_key'),
+          productFlowType: get(product, 'product_flow_type'),
+          stageId: get(product, 'stage_id'),
+          stageName: get(product, 'stage_name'),
+          listPrice: get(product, 'list_price'),
+          distributorPrice: get(product, 'distributor_price'),
+          mediaMainImage: get(product, 'media.main_image'),
+          mediaGallery: get(product, 'media.gallery'),
+          mediaDocuments: get(product, 'media.documents'),
+          mediaVideos: get(product, 'media.videos'),
+          // overviews: JSON.stringify(get(product, 'overviews', [])),
+          piecesPerBox: get(product, 'weights_and_dimensions.pieces_per_box'),
+          pieceHeight: get(product, 'weights_and_dimensions.piece_height'),
+          pieceLength: get(product, 'weights_and_dimensions.piece_length'),
+          pieceWidth: get(product, 'weights_and_dimensions.piece_width'),
+          pieceWeight: get(product, 'weights_and_dimensions.piece_weight'),
+          boxHeight: get(product, 'weights_and_dimensions.box_height'),
+          boxLength: get(product, 'weights_and_dimensions.box_length'),
+          boxWidth: get(product, 'weights_and_dimensions.box_width'),
+          boxWeight: get(product, 'weights_and_dimensions.box_weight'),
+          breadcrumb: get(product, 'category_breadcrumb'),
+          breadcrumbTree: JSON.stringify(
+            get(product, 'category_breadcrumb_tree', {})
+          ),
+          inventory: (get(product, 'inventory_detailed', []) as any[]).map(
+            item => ({
+              name: get(item, 'name'),
+              quantity: get(item, 'quantity'),
+              warehouseId: get(item, 'warehouse_id').toString(),
+              $id: [get(product, 'tvc_id'), get(item, 'warehouse_id')].join('-')
+            })
+          )
+        };
+
+        // log(payload);
+        if (
+          existingProducts.documents.some(
+            (doc: any) => doc.$id === payload.tvcId.toString()
+          )
+        ) {
+          log('updating product: ' + payload.tvcId);
+          await databases.updateDocument(
+            'tvc',
+            'products',
+            payload.tvcId.toString(),
+            payload
+          );
+        } else {
+          log('creating product: ' + payload.tvcId);
+          await databases.createDocument(
+            'tvc',
+            'products',
+            payload.tvcId.toString(),
+            payload
+          );
+        }
+      }
+
+      hasNextPage = json.links.next !== null;
+      CURRENT_PAGE++;
+      ATTEMPTS_COUNT = 1;
+      LOOP_COUNT++;
+    } catch (e) {
+      error(e);
+      log('Attempt: ' + ATTEMPTS_COUNT);
+      log('Current Page: ' + CURRENT_PAGE);
+      log('Loop Count: ' + LOOP_COUNT);
+      ATTEMPTS_COUNT++;
+
+      if (ATTEMPTS_COUNT > MAX_ATTEMPTS) {
+        error(e);
+        throw e;
+      }
+    }
+    currentTime = new Date().getTime();
+    log('time elapsed' + (currentTime - startTime) / 1000);
+  } while (
+    hasNextPage &&
+    ATTEMPTS_COUNT <= MAX_ATTEMPTS &&
+    LOOP_COUNT <= MAX_LOOP_TIMES &&
+    currentTime - startTime < 1000 * 60 * 3
+  );
+
+  if (
+    hasNextPage &&
+    ATTEMPTS_COUNT <= MAX_ATTEMPTS &&
+    LOOP_COUNT <= MAX_LOOP_TIMES
+  ) {
+    log("didn't finish, calling again with next page:" + CURRENT_PAGE);
+    functions.createExecution(
+      // @ts-ignore
+      Bun.env['APPWRITE_FUNCTION_ID'],
+      JSON.stringify({
+        startPage: CURRENT_PAGE
+      }),
+      true
+    );
+  }
+
+  return res.json({ ok: true });
+};
+
+export default main;
+
+main({
+  req: {},
+  res: { json: console.log },
+  log: console.log,
+  error: console.error
+});
